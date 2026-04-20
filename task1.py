2afa3cb73fe4477b1318076832c24c9a9094a77a
“Abdul-Salam <abdul-salam.adedayo@ibm.com>
2026-04-20 14:39:30 +0100

    task 1 with pytest

--- task1.py
+++ task1.py
@@ -1,1 +1,15 @@
-print("hello world")
+#Fizzbuzz
+# multiple of 3 fizz, multiple of 5 buzz, both FizzBuzz, none return error
+def fizzBuzz(num):
+    if ((num % 3) == 0) and ((num % 5) == 0):
+        return "FizzBuzz"
+    elif (num % 3) == 0:
+        return "Fizz"
+    elif (num % 5) == 0:
+        return "Buzz"
+    else:
+        # new_num = int(input("Type a new number"))
+        # return(fizzBuzz(new_num))
+        return None
+
+
